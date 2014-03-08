c204ca24986fdea2cc3a927f56a6e31be1626f18
Jonathan Tron <jonathan@tron.name>
2014-03-08 15:47:15 +0100

    Remove `outer` method and use `execCommand` for print

--- jquery.jqprint-0.3.js
+++ jquery.jqprint-0.3.js
@@ -6,6 +6,7 @@
 // - 11/05/2009 - first sketch
 //
 // - 05/07/2012 - Bug fix by The TalentBox Development Team: add all stylesheets
+// - 08/03/2014 - Bug fix by The TalentBox Development Team: remove uneeded 'outer' method, use execCommand for better print support
 //
 // Printing plug-in for jQuery, evolution of jPrintArea: http://plugins.jquery.com/project/jPrintArea
 // requires jQuery 1.3.x
@@ -20,48 +21,51 @@
         opt = $.extend({}, $.fn.jqprint.defaults, options);
 
         var $element = (this instanceof jQuery) ? this : $(this);
-        
-        if (opt.operaSupport && $.browser.opera) 
-        { 
+
+        if (opt.operaSupport && $.browser.opera)
+        {
             var tab = window.open("","jqPrint-preview");
             tab.document.open();
 
             var doc = tab.document;
         }
-        else 
+        else
         {
             var $iframe = $("<iframe  />");
-        
+
             if (!opt.debug) { $iframe.css({ position: "absolute", width: "0px", height: "0px", left: "-600px", top: "-600px" }); }
 
             $iframe.appendTo("body");
             var doc = $iframe[0].contentWindow.document;
         }
-        
+
         if (opt.importCSS) {
           $('link[rel="stylesheet"]').each( function() {
             doc.write("<link type='text/css' rel='stylesheet' href='" + $(this).attr("href") + "' media='" + $(this).attr("media") + "' />");
           });
         }
-        
-        if (opt.printContainer) { doc.write($element.outer()); }
-        else { $element.each( function() { doc.write($(this).html()); }); }
-        
-        doc.close();
-        
-        (opt.operaSupport && $.browser.opera ? tab : $iframe[0].contentWindow).focus();
-        setTimeout( function() { (opt.operaSupport && $.browser.opera ? tab : $iframe[0].contentWindow).print(); if (tab) { tab.close(); } }, 1000);
-    }
-    
-    $.fn.jqprint.defaults = {
-		debug: false,
-		importCSS: true, 
-		printContainer: true,
-		operaSupport: true
-	};
 
-    // Thanks to 9__, found at http://users.livejournal.com/9__/380664.html
-    jQuery.fn.outer = function() {
-      return $($('<div></div>').html(this.clone())).html();
-    } 
+        if (opt.printContainer) { doc.write($element.prop("outerHTML")); }
+        else { $element.each( function() { doc.write($(this).html()); }); }
+
+        doc.close();
+
+        (opt.operaSupport && $.browser.opera ? tab : $iframe[0].contentWindow).focus();
+        setTimeout( function() {
+          if (opt.operaSupport && $.browser.opera) {
+            tab.print();
+            tab.close();
+          } else {
+            doc.execCommand('print', false, null);
+          }
+        }, 1000);
+    }
+
+    $.fn.jqprint.defaults = {
+  		debug: false,
+  		importCSS: true,
+  		printContainer: true,
+  		operaSupport: true
+  	};
+
 })(jQuery);
